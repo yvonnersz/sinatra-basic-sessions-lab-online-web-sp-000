b22d0f708934f9372a3518cb277dcdd8335c727a
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-28 05:34:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+    erb :checkout
+
+  end
+
 end
